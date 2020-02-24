adfd851320c70f7efe1caf66879553f08763ab94
andrey <andreyzasulevichdev@gmail.com>
2020-02-24 19:35:19 +0300

    feat: add towel sort function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix) return [];
+    matrix.forEach((item, index) => {
+        if (index % 2 !== 0) return item.reverse();
+    });
+    return [].concat(...matrix);
+};
